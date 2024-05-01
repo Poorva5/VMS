b103923a675699dae8631f6b4b03e1c65a6d34a2
Poorva5 <poorvak455@gmail.com>
2024-05-01 21:38:18 +0530

    Added vendor apis

--- vendor_insights/admin.py
+++ vendor_insights/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
 
-# Register your models here.
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ("name", "vendor_code")
+
+
+admin.site.register(Vendor, VendorAdmin)
+
+
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "po_number", "status")
+
+
+admin.site.register(PurchaseOrder, PurchaseOrderAdmin)
+
+
+class HistoricalPerformanceAdmin(admin.ModelAdmin):
+    list_display = ("id", "date", "on_time_delivery_rate")
+
+
+admin.site.register(HistoricalPerformance, HistoricalPerformanceAdmin)
--- vendor_insights/views.py
+++ vendor_insights/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import (
+    VendorSerializer,
+    PurchaseOrderSerializer,
+    HistoricalPerformanceSerializer,
+)
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
+from rest_framework import viewsets
 
-# Create your views here.
+
+class VendorView(viewsets.ModelViewSet):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
